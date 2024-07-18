d1a2327e437c428dad8243e441c96f74ecde87db
shimonzhion <brys94204@gmail.com>
2024-07-18 04:33:25 +0300

    Create user schema model

---
+++ server/models/user-model.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const userScema = new mongoose.Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    image: String,
+    phone: String,
+    password: String,
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.model("users", userScema);
+module.exports = UserModel;
